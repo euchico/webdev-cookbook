c8efde7150829c1626ddd1b9aaa259f5bbc6c682
euchico <franciscodepaula.web@gmail.com>
2020-07-11 15:45:08 -0300

    Finalização da Template

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,17 @@ if (document.getElementById("note")){
     let element = document.getElementById("content");
     element.classList.toggle("show");
   }
+
+  function copyContent(e) {
+    var copyText = document.getElementById(e);
+    var textArea = document.createElement("textarea");
+
+    textArea.value = copyText.textContent;
+    document.body.appendChild(textArea);
+
+    textArea.select();
+    document.execCommand("copy");
+
+    textArea.remove();
+  }
 }
