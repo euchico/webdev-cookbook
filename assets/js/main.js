878dc530b300377ea4c55d97651a2883d85ac1ae
euchico <franciscodepaula.web@gmail.com>
2020-07-11 00:22:35 -0300

    Página de template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-if (!(document.getElementById("home"))){
+if (document.getElementById("note")){
   document.getElementById("menu-toggle").addEventListener("click", openMenu);
 
   function openMenu() {
-    let element = document.getElementById("wrapper");
-    element.classList.toggle("toggled");
+    let element = document.getElementById("content");
+    element.classList.toggle("show");
   }
 }
